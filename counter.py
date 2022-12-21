0c449f2557b58e2efc8fc57718953950de61aa35
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-21 14:15:27 -0600

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,10 +1,9 @@
-
 def countDayOfTheWeek():
     # This first line is provided for you
-    file_name = input("Enter a file name: ")    # end assignment
+    file_name = input("Enter a file name: ")
 
-## if you want to test locally before you try to sync
-## uncomment calculateAbsolute() and run > python payCalculator.py
-## ***IMPORTANT*** please recomment before you submit/sync your assignment.
-## OR YOUR TEST WILL NOT RUN
-##countDayOfTheWeek()
+
+
+## if you want to test locally run > python payCalculator.py
+if __name__ == "__main__":
+    countDayOfTheWeek()
